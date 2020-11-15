a43c44bfd04a1972d406a69fde19cc1fb546fe9c
projectcapstonea <73893395+projectcapstonea@users.noreply.github.com>
2020-11-14 22:50:05 -0500

    Update __init__.py

--- project/__init__.py
+++ project/__init__.py
@@ -2,9 +2,9 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 from flask import Flask
+import pyodbc
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-import pyodbc
 
 login_manager = LoginManager()
 
@@ -14,12 +14,11 @@ load_dotenv(path)
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-#drivers = [d for d in pyodbc.drivers()]
+drivers = [d for d in pyodbc.drivers()]
 
 SERVER = 'ifood-server.database.windows.net'
 DATABASE = 'iFood'
-#DRIVER = drivers[-1]
-DRIVER = 'SQL Server Native Client 11.0'
+DRIVER = drivers[-1]
 USERNAME = 'dbadmin'
 PASSWORD = os.getenv('DB_PASSWORD')
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mssql://{USERNAME}:{PASSWORD}@{SERVER}/{DATABASE}?driver={DRIVER}'
