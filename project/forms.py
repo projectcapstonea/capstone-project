055a55bfb63d5c83cf56000fdb615422e4dcc192
projectcapstonea <73893395+projectcapstonea@users.noreply.github.com>
2020-12-14 00:15:45 -0500

    Update forms.py

--- project/forms.py
+++ project/forms.py
@@ -1,10 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, SelectField, BooleanField
+from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired, Email, EqualTo
 from wtforms import ValidationError
+from project import db
+from project.models import CustomerData
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()]) # StringField('[label], list of instanced validators'
+    email = StringField('Email', validators=[DataRequired(), Email()]) # StringField('[label]', list of instanced validators')
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Log in')
 
@@ -14,17 +17,26 @@ class RegistrationForm(FlaskForm):
     confirm_pass = PasswordField('Confirm Password', validators=[DataRequired()])
     fname = StringField('First Name', validators=[DataRequired()])
     lname = StringField('Last Name', validators=[DataRequired()])
-    dob = StringField('DOB', validators=[DataRequired()])
+    dob = DateField('DOB', validators=[DataRequired()])
     str = StringField('Street', validators=[DataRequired()])
     cty = StringField('City', validators=[DataRequired()])
     trty = StringField('Territory', validators=[DataRequired()])
     ctry = StringField('Country', validators=[DataRequired()])
-    intr = StringField('Interests', validators=[DataRequired()])
-    pers = StringField('Personality', validators=[DataRequired()])
+    intr = SelectField('Interests', choices=[(q[0], q[0]) for q in db.session.query(CustomerData.int).distinct()])
+    pers = SelectField('Personality', choices=[(q[0], q[0]) for q in db.session.query(CustomerData.per).distinct()])
+    pref = BooleanField('Email Preference',validators=None)
     submit = SubmitField('Register')
 
     def check_email(self,field):
         if User.query.filter_by(email=field.data).first():
             raise ValidationError('Email has already been registered')
 
+class RecommendationForm(FlaskForm):
+    place = SelectField('Restaurant', choices=[(q[1], q[0]) for q in db.session.execute("SELECT name, placeID FROM RestaurantFeatureData ORDER BY name")])
+    rating = SelectField('Rating', choices=[('0', '0'), ('1', '1'), ('2', '2')])
+    submit = SubmitField('Submit')
 
+class RatingForm(FlaskForm):
+    restaurant = SelectField('Restaurant', choices=[(q[1], q[0]) for q in db.session.execute("SELECT name, placeID FROM RestaurantFeatureData ORDER BY name")])
+    rating = SelectField('Rating', choices=[('0', '0'), ('1', '1'), ('2', '2')])
+    submit = SubmitField('Submit')
